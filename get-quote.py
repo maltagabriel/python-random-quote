1b21b8662e80ff266c93c8ed27f7640f21328c54
Gabriel Malta <gabriel.malta@alumni.usp.br>
2022-03-07 11:44:25 -0300

    Writting new quotes

--- get-quote.py
+++ get-quote.py
@@ -3,13 +3,20 @@ import random
 def principal():
   ##print("Keep it logically awesome.")
 
+  f = open("quotes.txt", 'a')
+  f.write('Tentativa de adicionar uma quote \n')
+  f.close()
+
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   last = len(quotes)-1
   rnd = random.randint(0,last)
-  print(quotes[rnd])
+  print(quotes[rnd], end="")
+  ## essa opção de dizer como termina o print está disponível para o python3 e
+  ## substitui a newline padrão
+  print(quotes[last])
 
 if __name__== "__main__":
   principal()
